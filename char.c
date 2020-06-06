42f7c9e3dba44d32ab95127313ebd410f5625bdf
Kristine Legaspi <krissylegaspi@gmail.com>
2020-06-05 17:20:58 -0700

    Create char.c

---
+++ char.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+
+/****************************
+
+Your name: Kristine Legaspi
+
+****************************/
+
+int main()
+
+{
+
+    char name[10];
+    printf("Enter your name:");
+    scanf("%s", name);
+    printf("%s", name);
+    return 0;
+} 
